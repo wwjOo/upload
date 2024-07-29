01e3a0abda5e86d5b85d0b06ad84a8a9b3c9b8c1
qcloud <ubuntu@localhost.localdomain>
2024-07-29 15:34:34 +0800

    解决密码错误上传进度条显示的bug,完善密码管理,修改apache页面,增加header.html进行表格等页面美化

--- upload.php
+++ upload.php
@@ -3,13 +3,181 @@
 // 检查密码是否正确
 $publicUserPassword = "1234";
 $supperUserPassword = "0000";
-$defalutDir = "/home/ubuntu/web";
+$defalutDir = "/home/ubuntu/files";
+$linkHeadName = "header.html";
+$linkReadmeName = "upload.html";
+
+//删除文件夹函数
+function deleteDirectory($dir_path) {
+    if (!is_dir($dir_path)) {
+        return false;
+    }
+
+    $files = array_diff(scandir($dir_path), array('.', '..'));
+
+    foreach ($files as $file) {
+        $path = $dir_path . '/' . $file;
+
+        if (is_dir($path)) {
+            deleteDirectory($path);
+        } else {
+            unlink($path);
+        }
+    }
+
+    return rmdir($dir_path);
+}
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') 
 {
-    if(isset($_POST['password']) && isset($_POST['userInput']) && isset($_FILES['file'])){
-        HandleUpload($publicUserPassword,$supperUserPassword,$defalutDir);
+    //密码校验
+    if(isset($_POST['password']))
+    {
+        $password = $_POST['password'];
+        
+        // 检查密码是否正确
+        if ($password !== $publicUserPassword && $password !== $supperUserPassword) {
+            $response = array('status' => 'failed', 'message' => '权限验证失败');
+            echo json_encode($response);
+        }
+        // 密码正确后检查路径是否正确
+        else if(isset($_POST['filepath'])) {
+            $filepath = parse_url($_POST['filepath'], PHP_URL_PATH); //解析为相对ip地址的路径
+
+            $message = "权限验证通过\n";
+            $status = "success";
+
+            // 确保路径末尾有斜杠
+            if (substr($filepath, -1) !== '/') {
+                $filepath .= '/';
+            }
+            // 检查目录是否存在
+            $targetDir = $defalutDir . $filepath;
+            if (!is_dir($targetDir))
+            {
+                //超级用户可创建目录和软链接
+                if($password === $supperUserPassword) {
+                    $message .= "创建新目录:" . $filepath . "\n";
+                    mkdir($targetDir, 0755, true);
+
+                    $sourceFile = $defalutDir . '/' . $linkHeadName;
+                    $targetFile = $targetDir . '/' . $linkHeadName;
+                    if (symlink($sourceFile, $targetFile)){
+                        // $message .= "创建header软链接成功\n";
+                    } 
+                    else{
+                        $message .= "无法创建header软链接\n";
+                        $status = "failed";
+                    }
+                    
+                    $sourceFile = $defalutDir . '/' . $linkReadmeName;
+                    $targetFile = $targetDir . '/' . $linkReadmeName;
+                    if (symlink($sourceFile, $targetFile)){
+                        // $message .= "创建Readme软链接成功\n";
+                    } 
+                    else{
+                        $message .= "无法创建Readme软链接\n";
+                        $status = "failed";
+                    }
+                }
+                else {
+                    $message .= "目录不存在或无法访问:" . $filepath . "\n";
+                    $status = "failed";
+                }
+            }
+            $response = array('status' => $status, 'message' => $message, 'targetDir' => $targetDir);
+            echo json_encode($response);
+        }
+        //执行删除文件操作
+        else if(isset($_POST['rmfile']))
+        {
+            if($password === $supperUserPassword) {
+                $name = $defalutDir . $_POST['rmfile'];
+                $message = "";
+                $status = "success";
+    
+                //文件夹
+                if (is_dir($name)) {
+                    if(deleteDirectory($name)){
+                        $message .= "删除文件夹成功";
+                    }
+                    else{
+                        $message .= "删除文件夹失败";
+                        $status = "failed";
+                    }
+                }
+                //文件
+                else{
+                    if (file_exists($name)) {
+                        if (unlink($name)) {
+                            $message .= "文件删除成功";
+                        } else {
+                            $message .= "无法删除文件";
+                            $status = "failed";
+                        }
+                    } else {
+                        $message .= "文件不存在";
+                        $status = "failed";
+                    }
+                }
+
+                $response = array('status' => $status, 'message' => $message);
+                echo json_encode($response);
+            }
+            else{
+                $response = array('status' => 'success', 'message' => '您没有删除权限');
+                echo json_encode($response);
+            }
+        }
+        // 仅需验证密码且正确
+        else{
+            $response = array('status' => 'success', 'message' => '权限验证通过');
+            echo json_encode($response);
+        }
     }
+
+    //文件上传
+    if(isset($_FILES['file']) && isset($_POST['dir']))
+    {
+        //dir + filename
+        $targetFile = $_POST['dir'] . basename($_FILES['file']['name']);
+        // 上传
+        if (move_uploaded_file($_FILES['file']['tmp_name'], $targetFile)) 
+        {
+            echo "<h2>上传成功!</h2><h4>(5s后将自动跳转至首页)</h4>";
+
+            echo date('Y-m-d  H:i:s') . "<br>";
+            echo "上传文件名: " . $_FILES["file"]["name"] . "<br>";
+            echo "文件类型: " . $_FILES["file"]["type"] . "<br>";
+            echo "文件大小: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
+            echo "上传位置: " . $path;
+
+            header("refresh:5;url=$path");
+        } 
+        else 
+        {
+            if($_FILES["file"]["error"] == "1")
+            {
+                echo "上传的文件超过了设定的大小限制" . "<br>";
+
+                //这些参数在/etc/php/version/apache2/php.ini中修改
+                $uploadMaxFilesize = ini_get('upload_max_filesize');
+                $postMaxSize = ini_get('post_max_size');
+                echo "最大上传文件大小设置为: $uploadMaxFilesize<br>";
+                if($postMaxSize != "0")
+                    echo "POST 请求最大大小设置为: $postMaxSize<br>";
+            }
+            else
+            {
+                echo "上传文件时发生错误 code:" . $_FILES["file"]["error"] . "<br>";
+            }
+
+            header("refresh:5;url=" . $_SERVER['HTTP_REFERER']);
+        }
+    }
+
+
+
     // elseif(isset($_POST['userInput'])){ // 检查是否接收到名为 "userInput" 的表单字段
     //     HandleTest();
     // } 
@@ -67,75 +235,5 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST')
 //     echo "<p>您输入的文本是： $userInput </p>";
 //     echo "hello world";
 // }
-
-function HandleUpload($publicUserPassword,$supperUserPassword,$defalutDir)
-{
-    // 获取用户输入的密码
-    $password = $_POST['password'];
-    // 检查密码是否正确
-    if ($password !== $publicUserPassword && $password !== $supperUserPassword) 
-    {
-        //3s后返回上一级
-        header("refresh:3;url=" . $_SERVER['HTTP_REFERER']);
-        die("密码不正确，请返回重试！");
-    }
-
-    // 获取上传路径
-    $path = parse_url($_POST['userInput'], PHP_URL_PATH);
-    // 确保路径末尾有斜杠
-    if (substr($path, -1) !== '/') {
-        $path .= '/';
-    }
-    $targetDir = $defalutDir . $path;
-    if (!is_dir($targetDir)) 
-    {
-        //超级用户可创建目录
-        if($password === $supperUserPassword)
-        {
-            mkdir($targetDir, 0755, true);
-            echo "创建新目录: " . $targetDir;
-        }
-        else
-        {
-            echo $targetDir . "<br>";
-            die("目录不存在或无法访问!!!");
-        }
-    }
-
-    // 上传
-    $targetFile = $targetDir . basename($_FILES['file']['name']);
-    if (move_uploaded_file($_FILES['file']['tmp_name'], $targetFile)) 
-    {
-        echo "<h2>上传成功!</h2><h4>(5s后将自动跳转至上传的文件处)</h4>";
-
-        echo date('Y-m-d  H:i:s') . "<br>";
-        echo "上传文件名: " . $_FILES["file"]["name"] . "<br>";
-        echo "文件类型: " . $_FILES["file"]["type"] . "<br>";
-        echo "文件大小: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
-        echo "上传位置: " . $targetDir;
-
-        header("refresh:5;url=$path");
-    } 
-    else 
-    {
-        if($_FILES["file"]["error"] == "1")
-        {
-            echo "上传的文件超过了设定的大小限制" . "<br>";
-
-            //这些参数在/etc/php/version/apache2/php.ini中修改
-            $uploadMaxFilesize = ini_get('upload_max_filesize');
-            $postMaxSize = ini_get('post_max_size');
-            echo "最大上传文件大小设置为: $uploadMaxFilesize<br>";
-            if($postMaxSize != "0")
-                echo "POST 请求最大大小设置为: $postMaxSize<br>";
-        }
-        else
-        {
-            echo "上传文件时发生错误 code:" . $_FILES["file"]["error"] . "<br>";
-        }
-
-        header("refresh:5;url=" . $_SERVER['HTTP_REFERER']);
-    }
-}
 
 ?>
